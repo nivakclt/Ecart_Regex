3b247f461b4b9f3959180c2071fd42cc25aee310
nivakclt <nivakclt@gmail.com>
2026-08-13 21:51:00 +0530

    nav

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,7 +4,7 @@ function Footer() {
   return (
     <>
         {/* <!-- Footer--> */}
-        <footer class="py-5 bg-dark">
+        <footer class="py-5" style={{backgroundColor:"black"}}>
             <div class="container"><p class="m-0 text-center text-white">Copyright &copy; Your Website 2023</p></div>
         </footer>
     </>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,11 +5,11 @@ function Home() {
     <>
 
         {/* <!-- Header--> */}
-        <header className="bg-dark py-5">
+        <header className="py-5" style={{backgroundColor:"black"}}>
             <div className="container px-4 px-lg-5 my-5">
                 <div className="text-center text-white">
                     <h1 className="display-4 fw-bolder">Shop in style</h1>
-                    <p className="lead fw-normal text-white-50 mb-0">With this shop hompeage template</p>
+                    <p className="lead fw-normal text-white-50 mb-0">by your style</p>
                 </div>
             </div>
         </header>
